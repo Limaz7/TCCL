b35d89e90a36a8c04b0a2430190e4aaa39a5df31
Lázaro <161373444+Limaz7@users.noreply.github.com>
2024-08-22 14:33:06 -0300

    Add files via upload

---
+++ tcc (2).sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 22-Ago-2024 às 17:31
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `tcc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `enderecos`
+--
+
+DROP TABLE IF EXISTS `enderecos`;
+CREATE TABLE IF NOT EXISTS `enderecos` (
+  `id_endereco` int NOT NULL AUTO_INCREMENT,
+  `id_evento` int NOT NULL,
+  `cep` int NOT NULL,
+  `rua` varchar(255) NOT NULL,
+  `numero` int NOT NULL,
+  `bairro` varchar(255) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
+  `estado` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_endereco`),
+  KEY `fk_id_evento` (`id_evento`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `enderecos`
+--
+
+INSERT INTO `enderecos` (`id_endereco`, `id_evento`, `cep`, `rua`, `numero`, `bairro`, `cidade`, `estado`) VALUES
+(13, 37, 4643643, 'general camara', 4643, 'Vila julia', 'Uruguaiana', 'RS'),
+(19, 43, 4643643, 'general camara', 4643, 'Vila julia', 'Uruguaiana', 'RS'),
+(20, 44, 4643643, 'general camara', 4643, 'Vila julia', 'Uruguaiana', 'RS');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `eventos`
+--
+
+DROP TABLE IF EXISTS `eventos`;
+CREATE TABLE IF NOT EXISTS `eventos` (
+  `id_evento` int NOT NULL AUTO_INCREMENT,
+  `nome_evento` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `nome_empresa` varchar(255) NOT NULL,
+  `descricao` varchar(255) NOT NULL,
+  `data` datetime NOT NULL,
+  `imagem` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_evento`)
+) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `eventos`
+--
+
+INSERT INTO `eventos` (`id_evento`, `nome_evento`, `nome_empresa`, `descricao`, `data`, `imagem`) VALUES
+(37, 'SHOW DA BRUNA LOUISE', 'emp_Lazzarus', 'vai ser inedito', '2018-10-10 08:46:00', '66bb482e41099.jpeg'),
+(43, 'SHOW DA BRUNA LOUISE', 'emp_Lazzarus', 'vai ser inedito', '2018-10-10 08:46:00', '66bb484a8d7d3.jpeg'),
+(44, 'SHOW DA BRUNA LOUISE', 'emp_Lazzarus', 'vai ser inedito', '2018-10-10 08:46:00', '66bb484d34edc.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `ingressos`
+--
+
+DROP TABLE IF EXISTS `ingressos`;
+CREATE TABLE IF NOT EXISTS `ingressos` (
+  `id_ingresso` int NOT NULL AUTO_INCREMENT,
+  `valor` float NOT NULL,
+  `id_evento` int NOT NULL,
+  `id_participante` int NOT NULL,
+  PRIMARY KEY (`id_ingresso`),
+  KEY `fk_id_evento` (`id_evento`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tipos_usuario`
+--
+
+DROP TABLE IF EXISTS `tipos_usuario`;
+CREATE TABLE IF NOT EXISTS `tipos_usuario` (
+  `id_tipo_usuario` int NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_tipo_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `tipos_usuario`
+--
+
+INSERT INTO `tipos_usuario` (`id_tipo_usuario`, `descricao`) VALUES
+(1, 'adm'),
+(2, 'participante'),
+(3, 'empresa');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id_usuario` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `tipo_usuario` int NOT NULL,
+  `cod_ativacao` int NOT NULL,
+  PRIMARY KEY (`id_usuario`),
+  KEY `fk_tipo_usuario` (`tipo_usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nome`, `email`, `senha`, `tipo_usuario`, `cod_ativacao`) VALUES
+(2, 'emp_Lazzarus', 'lazaro.2022315968@aluno.iffar.edu.br', '$argon2i$v=19$m=65536,t=4,p=1$M3IyWnNsTVlyeUI1d0EzbA$s9BRI5uh5IGOFClxCdkIf0p6toNzbRNi4W+GsS5201g', 3, 2);
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `enderecos`
+--
+ALTER TABLE `enderecos`
+  ADD CONSTRAINT `fk_id_eventos` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id_evento`);
+
+--
+-- Limitadores para a tabela `ingressos`
+--
+ALTER TABLE `ingressos`
+  ADD CONSTRAINT `fk_id_evento` FOREIGN KEY (`id_evento`) REFERENCES `eventos` (`id_evento`);
+
+--
+-- Limitadores para a tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_tipo_usuario` FOREIGN KEY (`tipo_usuario`) REFERENCES `tipos_usuario` (`id_tipo_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
