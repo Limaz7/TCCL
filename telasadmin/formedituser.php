e9295f7b142e75dcd4525402d25e8ca67d3060d6
Lázaro <lazaro.2022315968@aluno.iffar.edu.br>
2024-11-21 11:28:36 -0300

    aaa

---
+++ telasadmin/formedituser.php
@@ -0,0 +1,23 @@
+<?php
+
+
+include ('../conexao.php');
+$conexao = conectar();
+
+$id = $_GET['id_usuario'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar usuarios</title>
+</head>
+<body>
+    <form action="editusers">
+        
+    </form>
+</body>
+</html>
--- telasadmin/inicioadm.php
+++ telasadmin/inicioadm.php
@@ -21,37 +21,29 @@ $result = executarSQL($conexao, $sql);
 
     <h1>Tela administrador</h1>
 
-    <form action="alteravalidacao.php" method="post">
-        <table style="text-align: center;">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Validação de entrada</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <?php foreach ($result as $results) { ?>
-                        <?php if ($results['id_usuario'] > 1) { ?>
-                            <td><?= $results['id_usuario'] ?></td>
-                            <td><?= $results['nome'] ?></td>
-                            <td><?= $results['email'] ?></td>
-                            <td>
-                                <select name="cod_atv">
-                                    <option value="1">1 - Acesso liberado</option>
-                                    <option value="2">2 - Em analise</option>
-                                    <option value="3">3 - Acesso negado</option>
-                                </select>
-                            </td>
-                        <?php } ?>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
-    </form>
-    <input type="submit" value="Salvar informações">
+    <table style="text-align: center;">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+                <th>Validação de entrada</th>
+                <th colspan="2">Opções</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <?php foreach ($result as $results) { ?>
+                    <td><?= $results['id_usuario'] ?></td>
+                    <td><?= $results['nome'] ?></td>
+                    <td><?= $results['email'] ?></td>
+                    <td><?= $results['cod_ativacao'] ?></td>
+                    <td><a href="formedituser?id_usuario=<?= $results['id_usuario']; ?>">Editar</a></td>
+                    <td><a href="">Excluir</a></td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
 
 
 
