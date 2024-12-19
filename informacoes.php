4953e2d6f893c84b616b18ac8a0511c748953c0d
Lázaro <lazaro.2022315968@aluno.iffar.edu.br>
2024-12-18 22:28:03 -0300

    desenvolvendo o sistema de compra de ingresso

--- informacoes.php
+++ informacoes.php
@@ -22,6 +22,8 @@ $sql_user = "SELECT * FROM usuarios WHERE id_usuario=" . $_SESSION['user'][0];
 $result1 = executarSQL($conexao, $sql_user);
 $usuario = mysqli_fetch_assoc($result1);
 
+$valores_ingressos = []; // Array para armazenar os valores
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -33,6 +35,7 @@ $usuario = mysqli_fetch_assoc($result1);
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <!-- <link rel="stylesheet" href="compraIngresso/style.css"> -->
 
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -53,30 +56,10 @@ $usuario = mysqli_fetch_assoc($result1);
         <?= $evento['produtora']; ?>
 
         <h5>Ingressos</h5>
-        <?php if ($usuario['tipo_usuario'] == 2) { ?>
-
-            <a style="background: black; color: white;" class="waves-effect waves-light btn modal-trigger" href='#modalIngressos'>Comprar ingressos</a>
-
-
-            <div id="modalIngressos" class="modal">
-                <div class="modal-content">
-                    <h4>Comprar ingresso</h4>
-                    <form action="Ingressos/compraringresso.php" method="post">
-                        <input type="hidden" name="id_ev" value="<?= $id ?>">
-                        <p>Número do ingresso <input type="text" name="id_in" required>
-                        <p>Quantidade <input type="text" name="qtd" required>
-                        <div class="modal-footer">
-                            <a href="#!" style="background: red;" class="modal-close waves-effect waves-red btn">Cancelar</a>
-                            <button style="background: green;" type="submit" class="waves-effect waves-green btn">Comprar</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        <?php } ?>
 
 
         <?php if ($usuario['tipo_usuario'] == 3 and $evento['id_usuario'] == $_SESSION['user'][0]) { ?>
-            <a style="background: black; color: white;" class="waves-effect waves-light btn modal-trigger" href='#modalCadastroIngresso'>Cadastrar ingressos</a> <br> 
+            <a style="background: black; color: white;" class="waves-effect waves-light btn modal-trigger" href='#modalCadastroIngresso'>Cadastrar ingressos</a> <br>
 
 
             <div id="modalCadastroIngresso" class="modal">
@@ -97,22 +80,42 @@ $usuario = mysqli_fetch_assoc($result1);
         <?php } ?>
 
 
-
-        <?php $result = executarSQL($conexao, $sql); ?>
-        <?php while ($ingressos = mysqli_fetch_assoc($result)) { ?>
+        <h2>Valor dos ingressos: <span id="valor-total">R$ 0,00</span></h2>
 
 
-            <?php if ($evento['id_ingresso']) { ?>
+        <form action="compraingresso/compraringresso.php" method="post">
 
-                <br> Número de série: <?= $ingressos['id_ingresso'] . "<br>"; ?>
-                Descrição: <?= $ingressos['informacao'] . "<br>"; ?>
-                Valor do ingresso: <?= $ingressos['valor'] . "<br>"; ?>
-                Quantidade de ingressos restantes: <?= $ingressos['quantidade'] . "<br>"; ?> <br>
-
-            <?php } ?>
+            <?php $result = executarSQL($conexao, $sql); ?>
+            <?php $i = 0;
+            while ($ingressos = mysqli_fetch_assoc($result)) : ?>
 
 
-        <?php } ?>
+                <?php if ($evento['id_ingresso']) { ?>
+
+
+                    <?php $valores_ingressos[] = $ingressos['valor']; ?> <!-- Adiciona o valor ao array -->
+
+                    <div class="ingresso">
+                        <h5><?= $ingressos['informacao'] ?></h5>
+                        <p>1º Lote Unissex OPEN BAR R$ <?= number_format($ingressos['valor'], 2, ',', '.'); ?></p>
+                        <div class="quantidade">
+                            <button type="button" class="decremento" onclick="alterarQuantidade(-1, <?= $i; ?>)">-</button>
+                            <span id="qtd-<?= $i; ?>">0</span>
+                            <button type="button" class="incremento" onclick="alterarQuantidade(1, <?= $i; ?>)">+</button>
+                        </div>
+                    </div>
+
+
+
+
+                <?php } ?>
+
+            <?php $i++;
+            endwhile; ?>
+
+            <input type="submit" value="Enviar">
+
+        </form>
 
         <h3> Avaliações </h3>
 
@@ -145,5 +148,14 @@ $usuario = mysqli_fetch_assoc($result1);
     });
 </script>
 
+
+<!-- Passando o array PHP para o JavaScript -->
+<script>
+    const precos = <?php echo json_encode($valores_ingressos); ?>; // Array com os valores dos ingressos
+    console.log(precos); // Debug: Confirmação dos valores no console
+</script>
+
+
+<script src="compraIngresso/script.js"></script>
 
 </html>
